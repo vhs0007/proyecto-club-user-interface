e860a14bd42983183f22e9a6ff23f19b17b69913
vhs0007 <itzvalegamer12@gmail.com>
2026-04-05 21:34:59 -0300

    y bueno

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -2,7 +2,7 @@ import axios, { AxiosHeaders } from 'axios';
 import { useAuthStore, useClubIdStore } from '../store/store';
 
 const AxiosInstance = axios.create({
-  baseURL: 'http://localhost:3000',
+  baseURL: import.meta.env.VITE_API_URL ?? '',
   headers: {
     'Content-Type': 'application/json',
   },
